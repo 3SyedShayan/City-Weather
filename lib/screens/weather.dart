ea50a8936a880020279892ea91ff5e051b22ea2a
Syed Shayan <3syedshayan@gmail.com>
2025-03-17 23:27:42 +0500

    fetching Weather

--- lib/screens/weather.dart
+++ lib/screens/weather.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:my_city_weather/services/weather_service.dart';
 
-class Weather extends StatelessWidget {
-  const Weather({super.key});
+class Weather extends StatefulWidget {
+  Weather({super.key});
+  @override
+  State<Weather> createState() => _WeatherState();
+}
+
+class _WeatherState extends State<Weather> {
+  final _weatherService = WeatherService('There is Something');
+  Weather? _weather;
+  _fetchWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather as Weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _fetchWeather();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Weather App"),
+        child: Text(_weather?.cityName ?? "City Name.."),
       ),
     );
   }
